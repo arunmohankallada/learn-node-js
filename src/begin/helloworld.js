e5220000020ea0be279ebf11eb817d6cfb4df9d0
Arun Mohan <arun3210@gmail.com>
2019-01-10 16:01:37 -0600

    Update helloworld.js

--- src/begin/helloworld.js
+++ src/begin/helloworld.js
@@ -1,1 +1,15 @@
 console.log("Begining Of My Program");
+
+var sayHello = function(name){
+  var message = "Hello, " + name ;
+  console.log(message);
+}
+
+sayHello('Arun');
+
+/*
+Math.* has all the math function
+process.* - argv, and other system variables.
+*/
+
+console.log("Ending Of My Program");
