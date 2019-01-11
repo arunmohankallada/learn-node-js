5684a74bd7a14020a6b14c0e73e5aa2bf6a87e31
Arun Mohan <arun3210@gmail.com>
2019-01-10 18:06:00 -0600

    Initial Commit

---
+++ src/begin/myfirstObject.js
@@ -0,0 +1,12 @@
+var myFirstObject = {
+size:4,
+value:'arun',
+print: function(){
+  console.log("Size = "+ this.size + ", Value = " + this.value);
+}
+};
+
+console.log(myFirstObject);
+console.log(myFirstObject.size)
+console.log(myFirstObject.value)
+myFirstObject.print();
